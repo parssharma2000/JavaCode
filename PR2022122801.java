0d68f933a12cb3bc90a488141c3bd7b225895e50
parssharma2000 <51741809+parssharma2000@users.noreply.github.com>
2022-12-28 21:03:41 +0530

    PR2022122801

---
+++ PR2022122801.java
@@ -0,0 +1,22 @@
+
+public class PR2022122801 {
+
+	public static void main(String[] args) {
+		
+		int minVal = Integer.MIN_VALUE;
+		int maxVal = Integer.MAX_VALUE;
+		
+		//minVal=minVal-2;
+		
+		//maxVal=maxVal+2;
+		
+		long longInt = 2147483648L;
+		
+		System.out.println("Min value :"+minVal+", Max value :"+maxVal);
+		System.out.println("Byte Min value :"+Byte.MIN_VALUE+", Max value :"+Byte.MAX_VALUE);
+		System.out.println("Short value :"+Short.MIN_VALUE+", Max value :"+Short.MAX_VALUE);
+		System.out.println("Long Min value"+Long.MIN_VALUE+", Max value :"+Long.MAX_VALUE);
+
+	}
+
+}
